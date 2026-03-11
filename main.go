9260ab549e8c1091c13053b0efa673fcc9c79a1f
alexmaisa <alexmaisa@proton.me>
2026-03-11 16:03:27 +0700

    UI: Refactor Speed Units to use modern Radio Button groups

--- main.go
+++ main.go
@@ -66,10 +66,10 @@ func initDB() {
 
 	// Default settings
 	defaults := map[string]string{
-		"timezone":    "UTC",
-		"wan_unit":    "Mbps",
-		"lan_unit":    "Mbps",
-		"mask_mac":    "false",
+		"timezone": "UTC",
+		"wan_unit": "Mbps",
+		"lan_unit": "Mbps",
+		"mask_mac": "false",
 	}
 	for k, v := range defaults {
 		db.Exec(`INSERT OR IGNORE INTO settings (key, value) VALUES (?, ?);`, k, v)
@@ -200,12 +200,12 @@ func getMACAddress(ip string) string {
 	if err != nil {
 		return "Unknown MAC"
 	}
-	
+
 	// Example arp output: "? (192.168.1.5) at a1:b2:c3:d4:e5:f6 on en0 ifscope [ethernet]"
 	// Extract MAC using regex
 	re := regexp.MustCompile(`([0-9a-fA-F]{1,2}[:-]){5}([0-9a-fA-F]{1,2})`)
 	match := re.FindString(string(out))
-	
+
 	if match != "" {
 		return strings.ToLower(match)
 	}
@@ -213,12 +213,12 @@ func getMACAddress(ip string) string {
 }
 
 type LANSaveRequest struct {
-	Ping           float64 `json:"ping"`
-	Jitter         float64 `json:"jitter"`
-	MinPing        float64 `json:"min_ping"`
-	MaxPing        float64 `json:"max_ping"`
-	DownloadMbps   float64 `json:"download"`
-	UploadMbps     float64 `json:"upload"`
+	Ping         float64 `json:"ping"`
+	Jitter       float64 `json:"jitter"`
+	MinPing      float64 `json:"min_ping"`
+	MaxPing      float64 `json:"max_ping"`
+	DownloadMbps float64 `json:"download"`
+	UploadMbps   float64 `json:"upload"`
 }
 
 func handleLANSave(w http.ResponseWriter, r *http.Request) {
@@ -240,7 +240,7 @@ func handleLANSave(w http.ResponseWriter, r *http.Request) {
 		INSERT INTO lan_history (ip_address, mac_address, ping_ms, jitter_ms, min_ping_ms, max_ping_ms, download_mbps, upload_mbps) 
 		VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 	`, ip, mac, req.Ping, req.Jitter, req.MinPing, req.MaxPing, req.DownloadMbps, req.UploadMbps)
-	
+
 	if err != nil {
 		log.Printf("Failed to save LAN history: %v", err)
 		http.Error(w, "DB Error", http.StatusInternalServerError)
@@ -533,45 +533,45 @@ func handleTimezones(w http.ResponseWriter, r *http.Request) {
 
 var allTimezones = []string{
 	"UTC",
-	"Africa/Abidjan", "Africa/Accra", "Africa/Addis_Ababa", "Africa/Algiers", "Africa/Asmara",
+	"Africa/Abidjan", "Africa/Accra", "Africa/Addis Ababa", "Africa/Algiers", "Africa/Asmara",
 	"Africa/Bamako", "Africa/Bangui", "Africa/Banjul", "Africa/Bissau", "Africa/Blantyre",
 	"Africa/Brazzaville", "Africa/Bujumbura", "Africa/Cairo", "Africa/Casablanca", "Africa/Ceuta",
-	"Africa/Conakry", "Africa/Dakar", "Africa/Dar_es_Salaam", "Africa/Djibouti", "Africa/Douala",
-	"Africa/El_Aaiun", "Africa/Freetown", "Africa/Gaborone", "Africa/Harare", "Africa/Johannesburg",
+	"Africa/Conakry", "Africa/Dakar", "Africa/Dar es Salaam", "Africa/Djibouti", "Africa/Douala",
+	"Africa/El Aaiun", "Africa/Freetown", "Africa/Gaborone", "Africa/Harare", "Africa/Johannesburg",
 	"Africa/Juba", "Africa/Kampala", "Africa/Khartoum", "Africa/Kigali", "Africa/Kinshasa",
 	"Africa/Lagos", "Africa/Libreville", "Africa/Lome", "Africa/Luanda", "Africa/Lubumbashi",
 	"Africa/Lusaka", "Africa/Malabo", "Africa/Maputo", "Africa/Maseru", "Africa/Mbabane",
 	"Africa/Mogadishu", "Africa/Monrovia", "Africa/Nairobi", "Africa/Ndjamena", "Africa/Niamey",
-	"Africa/Nouakchott", "Africa/Ouagadougou", "Africa/Porto-Novo", "Africa/Sao_Tome", "Africa/Tripoli",
+	"Africa/Nouakchott", "Africa/Ouagadougou", "Africa/Porto-Novo", "Africa/Sao Tome", "Africa/Tripoli",
 	"Africa/Tunis", "Africa/Windhoek",
 	"America/Adak", "America/Anchorage", "America/Anguilla", "America/Antigua", "America/Araguaina",
-	"America/Argentina/Buenos_Aires", "America/Argentina/Catamarca", "America/Argentina/Cordoba", "America/Argentina/Jujuy", "America/Argentina/La_Rioja",
-	"America/Argentina/Mendoza", "America/Argentina/Rio_Gallegos", "America/Argentina/Salta", "America/Argentina/San_Juan", "America/Argentina/San_Luis",
+	"America/Argentina/Buenos Aires", "America/Argentina/Catamarca", "America/Argentina/Cordoba", "America/Argentina/Jujuy", "America/Argentina/La_Rioja",
+	"America/Argentina/Mendoza", "America/Argentina/Rio Gallegos", "America/Argentina/Salta", "America/Argentina/San_Juan", "America/Argentina/San_Luis",
 	"America/Argentina/Tucuman", "America/Argentina/Ushuaia", "America/Aruba", "America/Asuncion", "America/Atikokan",
-	"America/Bahia", "America/Bahia_Banderas", "America/Barbados", "America/Belem", "America/Belize",
-	"America/Blanc-Sablon", "America/Boa_Vista", "America/Bogota", "America/Boise", "America/Cambridge_Bay",
-	"America/Campo_Grande", "America/Cancun", "America/Caracas", "America/Cayenne", "America/Cayman",
-	"America/Chicago", "America/Chihuahua", "America/Costa_Rica", "America/Creston", "America/Cuiaba",
-	"America/Curacao", "America/Danmarkshavn", "America/Dawson", "America/Dawson_Creek", "America/Denver",
-	"America/Detroit", "America/Dominica", "America/Edmonton", "America/Eirunepe", "America/El_Salvador",
-	"America/Fort_Nelson", "America/Fortaleza", "America/Glace_Bay", "America/Goose_Bay", "America/Grand_Turk",
+	"America/Bahia", "America/Bahia Banderas", "America/Barbados", "America/Belem", "America/Belize",
+	"America/Blanc-Sablon", "America/Boa Vista", "America/Bogota", "America/Boise", "America/Cambridge Bay",
+	"America/Campo Grande", "America/Cancun", "America/Caracas", "America/Cayenne", "America/Cayman",
+	"America/Chicago", "America/Chihuahua", "America/Costa Rica", "America/Creston", "America/Cuiaba",
+	"America/Curacao", "America/Danmarkshavn", "America/Dawson", "America/Dawson Creek", "America/Denver",
+	"America/Detroit", "America/Dominica", "America/Edmonton", "America/Eirunepe", "America/El Salvador",
+	"America/Fort Nelson", "America/Fortaleza", "America/Glace Bay", "America/Goose Bay", "America/Grand Turk",
 	"America/Grenada", "America/Guadeloupe", "America/Guatemala", "America/Guayaquil", "America/Guyana",
 	"America/Halifax", "America/Havana", "America/Hermosillo", "America/Indiana/Indianapolis", "America/Indiana/Knox",
-	"America/Indiana/Marengo", "America/Indiana/Petersburg", "America/Indiana/Tell_City", "America/Indiana/Vevay", "America/Indiana/Vincennes",
+	"America/Indiana/Marengo", "America/Indiana/Petersburg", "America/Indiana/Tell City", "America/Indiana/Vevay", "America/Indiana/Vincennes",
 	"America/Indiana/Winamac", "America/Inuvik", "America/Iqaluit", "America/Jamaica", "America/Juneau",
-	"America/Kentucky/Louisville", "America/Kentucky/Monticello", "America/Kralendijk", "America/La_Paz", "America/Lima",
-	"America/Los_Angeles", "America/Lower_Princes", "America/Maceio", "America/Managua", "America/Manaus",
+	"America/Kentucky/Louisville", "America/Kentucky/Monticello", "America/Kralendijk", "America/La Paz", "America/Lima",
+	"America/Los Angeles", "America/Lower Princes", "America/Maceio", "America/Managua", "America/Manaus",
 	"America/Marigot", "America/Martinique", "America/Matamoros", "America/Mazatlan", "America/Menominee",
-	"America/Merida", "America/Metlakatla", "America/Mexico_City", "America/Miquelon", "America/Moncton",
-	"America/Monterrey", "America/Montevideo", "America/Montserrat", "America/Nassau", "America/New_York",
-	"America/Nipigon", "America/Nome", "America/Noronha", "America/North_Dakota/Beulah", "America/North_Dakota/Center",
-	"America/North_Dakota/New_Salem", "America/Nuuk", "America/Ojinaga", "America/Panama", "America/Pangnirtung",
-	"America/Paramaribo", "America/Phoenix", "America/Port-au-Prince", "America/Port_of_Spain", "America/Porto_Velho",
-	"America/Puerto_Rico", "America/Punta_Arenas", "America/Rainy_River", "America/Rankin_Inlet", "America/Recife",
-	"America/Regina", "America/Resolute", "America/Rio_Branco", "America/Santarem", "America/Santiago",
-	"America/Santo_Domingo", "America/Sao_Paulo", "America/Scoresbysund", "America/Sitka", "America/St_Barthelemy",
-	"America/St_Johns", "America/St_Kitts", "America/St_Lucia", "America/St_Thomas", "America/St_Vincent",
-	"America/Swift_Current", "America/Tegucigalpa", "America/Thule", "America/Thunder_Bay", "America/Tijuana",
+	"America/Merida", "America/Metlakatla", "America/Mexico City", "America/Miquelon", "America/Moncton",
+	"America/Monterrey", "America/Montevideo", "America/Montserrat", "America/Nassau", "America/New York",
+	"America/Nipigon", "America/Nome", "America/Noronha", "America/North Dakota/Beulah", "America/North Dakota/Center",
+	"America/North Dakota/New Salem", "America/Nuuk", "America/Ojinaga", "America/Panama", "America/Pangnirtung",
+	"America/Paramaribo", "America/Phoenix", "America/Port-au-Prince", "America/Port of Spain", "America/Porto Velho",
+	"America/Puerto Rico", "America/Punta Arenas", "America/Rainy River", "America/Rankin Inlet", "America/Recife",
+	"America/Regina", "America/Resolute", "America/Rio Branco", "America/Santarem", "America/Santiago",
+	"America/Santo Domingo", "America/Sao Paulo", "America/Scoresbysund", "America/Sitka", "America/St Barthelemy",
+	"America/St Johns", "America/St Kitts", "America/St Lucia", "America/St Thomas", "America/St Vincent",
+	"America/Swift Current", "America/Tegucigalpa", "America/Thule", "America/Thunder Bay", "America/Tijuana",
 	"America/Toronto", "America/Tortola", "America/Vancouver", "America/Whitehorse", "America/Winnipeg",
 	"America/Yakutat", "America/Yellowknife",
 	"Antarctica/Casey", "Antarctica/Davis", "Antarctica/DumontDUrville", "Antarctica/Mawson", "Antarctica/McMurdo",
@@ -581,32 +581,32 @@ var allTimezones = []string{
 	"Asia/Baku", "Asia/Bangkok", "Asia/Barnaul", "Asia/Beirut", "Asia/Bishkek",
 	"Asia/Brunei", "Asia/Chita", "Asia/Choibalsan", "Asia/Colombo", "Asia/Damascus",
 	"Asia/Dhaka", "Asia/Dili", "Asia/Dubai", "Asia/Dushanbe", "Asia/Famagusta",
-	"Asia/Gaza", "Asia/Hebron", "Asia/Ho_Chi_Minh", "Asia/Hong_Kong", "Asia/Hovd",
+	"Asia/Gaza", "Asia/Hebron", "Asia/Ho Chi Minh", "Asia/Hong Kong", "Asia/Hovd",
 	"Asia/Irkutsk", "Asia/Jakarta", "Asia/Jayapura", "Asia/Jerusalem", "Asia/Kabul",
 	"Asia/Kamchatka", "Asia/Karachi", "Asia/Kathmandu", "Asia/Khandyga", "Asia/Kolkata",
-	"Asia/Krasnoyarsk", "Asia/Kuala_Lumpur", "Asia/Kuching", "Asia/Kuwait", "Asia/Macau",
+	"Asia/Krasnoyarsk", "Asia/Kuala Lumpur", "Asia/Kuching", "Asia/Kuwait", "Asia/Macau",
 	"Asia/Magadan", "Asia/Makassar", "Asia/Manila", "Asia/Muscat", "Asia/Nicosia",
-	"Asia/Novokuznetsk", "Asia/Novosibirsk", "Asia/Omsk", "Asia/Oral", "Asia/Phnom_Penh",
+	"Asia/Novokuznetsk", "Asia/Novosibirsk", "Asia/Omsk", "Asia/Oral", "Asia/Phnom Penh",
 	"Asia/Pontianak", "Asia/Pyongyang", "Asia/Qatar", "Asia/Qostanay", "Asia/Qyzylorda",
 	"Asia/Riyadh", "Asia/Sakhalin", "Asia/Samarkand", "Asia/Seoul", "Asia/Shanghai",
 	"Asia/Singapore", "Asia/Srednekolymsk", "Asia/Taipei", "Asia/Tashkent", "Asia/Tbilisi",
 	"Asia/Tehran", "Asia/Thimphu", "Asia/Tokyo", "Asia/Tomsk", "Asia/Ulaanbaatar",
 	"Asia/Urumqi", "Asia/Ust-Nera", "Asia/Vientiane", "Asia/Vladivostok", "Asia/Yakutsk",
 	"Asia/Yangon", "Asia/Yekaterinburg", "Asia/Yerevan",
-	"Atlantic/Azores", "Atlantic/Bermuda", "Atlantic/Canary", "Atlantic/Cape_Verde", "Atlantic/Faroe",
-	"Atlantic/Madeira", "Atlantic/Reykjavik", "Atlantic/South_Georgia", "Atlantic/St_Helena", "Atlantic/Stanley",
-	"Australia/Adelaide", "Australia/Brisbane", "Australia/Broken_Hill", "Australia/Darwin", "Australia/Eucla",
-	"Australia/Hobart", "Australia/Lindeman", "Australia/Lord_Howe", "Australia/Melbourne", "Australia/Perth",
+	"Atlantic/Azores", "Atlantic/Bermuda", "Atlantic/Canary", "Atlantic/Cape Verde", "Atlantic/Faroe",
+	"Atlantic/Madeira", "Atlantic/Reykjavik", "Atlantic/South Georgia", "Atlantic/St Helena", "Atlantic/Stanley",
+	"Australia/Adelaide", "Australia/Brisbane", "Australia/Broken Hill", "Australia/Darwin", "Australia/Eucla",
+	"Australia/Hobart", "Australia/Lindeman", "Australia/Lord Howe", "Australia/Melbourne", "Australia/Perth",
 	"Australia/Sydney",
 	"Europe/Amsterdam", "Europe/Andorra", "Europe/Astrakhan", "Europe/Athens", "Europe/Belgrade",
 	"Europe/Berlin", "Europe/Bratislava", "Europe/Brussels", "Europe/Bucharest", "Europe/Budapest",
 	"Europe/Busingen", "Europe/Chisinau", "Europe/Copenhagen", "Europe/Dublin", "Europe/Gibraltar",
-	"Europe/Guernsey", "Europe/Helsinki", "Europe/Isle_of_Man", "Europe/Istanbul", "Europe/Jersey",
+	"Europe/Guernsey", "Europe/Helsinki", "Europe/Isle of Man", "Europe/Istanbul", "Europe/Jersey",
 	"Europe/Kaliningrad", "Europe/Kiev", "Europe/Kirov", "Europe/Lisbon", "Europe/Ljubljana",
 	"Europe/London", "Europe/Luxembourg", "Europe/Madrid", "Europe/Malta", "Europe/Mariehamn",
 	"Europe/Minsk", "Europe/Monaco", "Europe/Moscow", "Europe/Oslo", "Europe/Paris",
 	"Europe/Podgorica", "Europe/Prague", "Europe/Riga", "Europe/Rome", "Europe/Samara",
-	"Europe/San_Marino", "Europe/Sarajevo", "Europe/Saratov", "Europe/Simferopol", "Europe/Skopje",
+	"Europe/San Marino", "Europe/Sarajevo", "Europe/Saratov", "Europe/Simferopol", "Europe/Skopje",
 	"Europe/Sofia", "Europe/Stockholm", "Europe/Tallinn", "Europe/Tirane", "Europe/Ulyanovsk",
 	"Europe/Uzhgorod", "Europe/Vaduz", "Europe/Vatican", "Europe/Vienna", "Europe/Vilnius",
 	"Europe/Volgograd", "Europe/Warsaw", "Europe/Zagreb", "Europe/Zaporozhye", "Europe/Zurich",
@@ -618,7 +618,7 @@ var allTimezones = []string{
 	"Pacific/Galapagos", "Pacific/Gambier", "Pacific/Guadalcanal", "Pacific/Guam", "Pacific/Honolulu",
 	"Pacific/Kanton", "Pacific/Kiritimati", "Pacific/Kosrae", "Pacific/Kwajalein", "Pacific/Majuro",
 	"Pacific/Marquesas", "Pacific/Midway", "Pacific/Nauru", "Pacific/Niue", "Pacific/Norfolk",
-	"Pacific/Noumea", "Pacific/Pago_Pago", "Pacific/Palau", "Pacific/Pitcairn", "Pacific/Pohnpei",
-	"Pacific/Port_Moresby", "Pacific/Rarotonga", "Pacific/Saipan", "Pacific/Tahiti", "Pacific/Tarawa",
+	"Pacific/Noumea", "Pacific/Pago Pago", "Pacific/Palau", "Pacific/Pitcairn", "Pacific/Pohnpei",
+	"Pacific/Port Moresby", "Pacific/Rarotonga", "Pacific/Saipan", "Pacific/Tahiti", "Pacific/Tarawa",
 	"Pacific/Tongatapu", "Pacific/Wake", "Pacific/Wallis",
 }
